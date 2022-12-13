1acd5be5e40982daa074a6bb5e4c4ad1d9840fa4
wusaby-rush <rush@wusaby.me>
2022-12-13 18:20:06 +0300

    Add frontend project to repo

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 // app.use(morgan('tiny'))
