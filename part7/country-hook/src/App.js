2da1e0a5674c099334539c0a458839665b877880
rush <rush@wusaby.com>
2023-03-15 13:25:03 +0300

    Complete excercise 7.7

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((response) => response.json())
+      .then((data) => {
+        const country = data.filter((country) => country.name.common === name)
+        console.log('country', country[0])
+        setCountry({
+          found: country.length === 1,
+          data: {
+            name: country[0].name.common,
+            capital: country[0].capital[0],
+            population: country[0].population,
+            flags: country[0].flags
+          }
+        })
+      })
+  }, [name])
+
+  return country
+}
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.data.name} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flags.png} height='100' alt={country.data.flags.alt} />  
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
