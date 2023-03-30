f47989a052e5021b1db0c913f67270114f5f6c00
rush <rush@wusaby.com>
2023-03-30 22:05:46 +0300

    Add users pages and routes

--- part7/bloglist-frontend/src/App.jsx
+++ part7/bloglist-frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Notification from './components/Notification'
 import Login from './pages/Login'
 import Blogs from './pages/Blogs'
+import Users from './pages/Users'
+import User from './pages/User'
 import MainLayout from './layouts/MainLayout'
 
 const router = createBrowserRouter([
@@ -10,6 +12,8 @@ const router = createBrowserRouter([
     element: <MainLayout />,
     children: [
       { path: '/', element: <Blogs /> },
+      { path: '/users', element: <Users /> },
+      { path: '/users/:id', element: <User /> },
       { path: '/login', element: <Login /> },
     ],
   },
