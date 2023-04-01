0d02504b348f8ff382791b7ce094278bdbaa9d83
rush <rush@wusaby.com>
2023-04-01 21:46:53 +0300

    Move post form to blogs action

--- part7/bloglist-frontend/src/components/PostForm.jsx
+++ part7/bloglist-frontend/src/components/PostForm.jsx
@@ -1,42 +1,17 @@
-import { useMutation, useQueryClient } from 'react-query'
-import { useDispatch } from 'react-redux'
-import { setNotificationWithTimeout } from '../reducers/notificationReducer'
-import blogService from '../services/blogService'
+import { Form } from 'react-router-dom'
 
 function PostForm({ formRef }) {
-  const dispatch = useDispatch()
-  const queryClient = useQueryClient()
-  const { mutate } = useMutation(blogService.create, {
-    onSuccess: (data) => {
-      queryClient.setQueryData('blogs', (old) => [...old, data])
-      dispatch(setNotificationWithTimeout(`a new blog ${data.title} added`))
-    },
-    onError: (error) => {
-      dispatch(setNotificationWithTimeout(error.message, 'error'))
-    },
-  })
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    const blogObject = {
-      title: e.target.title.value,
-      author: e.target.author.value,
-      url: e.target.url.value,
-    }
-    mutate(blogObject)
-    formRef.current.toggleVisability()
-    e.target.title.value = ''
-    e.target.author.value = ''
-    e.target.url.value = ''
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
+    <Form
+      action="post"
+      method="/"
+      onSubmit={() => formRef.current.toggleVisability()}
+    >
       title: <input type="text" name="title" /> <br />
       author: <input type="text" name="author" /> <br />
       url: <input type="text" name="url" /> <br />
       <input type="submit" value="create" />
-    </form>
+    </Form>
   )
 }
 
--- part7/bloglist-frontend/src/pages/Blogs.jsx
+++ part7/bloglist-frontend/src/pages/Blogs.jsx
@@ -1,7 +1,33 @@
 import { useRef } from 'react'
+import { useMutation, useQueryClient } from 'react-query'
+import { useDispatch } from 'react-redux'
 import BlogList from '../components/BlogList'
 import Togglable from '../components/Togglable'
 import PostForm from '../components/PostForm'
+import { setNotificationWithTimeout } from '../reducers/notificationReducer'
+import blogService from '../services/blogService'
+
+export async function blogsAction({ request }) {
+  const dispatch = useDispatch()
+  const queryClient = useQueryClient()
+  const { mutate } = useMutation(blogService.create, {
+    onSuccess: (data) => {
+      queryClient.setQueryData('blogs', (old) => [...old, data])
+      dispatch(setNotificationWithTimeout(`a new blog ${data.title} added`))
+    },
+    onError: (error) => {
+      dispatch(setNotificationWithTimeout(error.message, 'error'))
+    },
+  })
+
+  const data = await request.formData('/')
+  const blogObject = {
+    title: data.get('title'),
+    author: data.get('author'),
+    url: data.get('url'),
+  }
+  mutate(blogObject)
+}
 
 export default function Blogs() {
   const blogFormRef = useRef()
