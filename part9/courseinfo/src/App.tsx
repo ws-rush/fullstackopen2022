bf1454e4ae9554ca61e347dc3cd2b563bd27bf40
wusaby-rush <rush@wusaby.com>
2023-06-05 14:01:05 +0300

    Patienor frontend

--- part9/courseinfo/src/App.tsx
+++ part9/courseinfo/src/App.tsx
@@ -1,32 +1,37 @@
 import Content from "./Content";
 import Header from "./Header";
 import Total from "./Total";
+import { CoursePart } from "./types";
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
-      description: "This is an awesome course part"
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
-      groupProjectCount: 3
+      groupProjectCount: 3,
+      kind: "group"
     },
     {
       name: "Basics of type Narrowing",
       exerciseCount: 7,
-      description: "How to go from unknown to string"
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
       description: "Confusing description",
-      backgroundMaterial: "https://type-level-typescript.com/template-literal-types"
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
     },
-  ];
+  ]
 
   return (
     <div>
--- part9/courseinfo/src/types.ts
+++ part9/courseinfo/src/types.ts
@@ -1,4 +1,22 @@
-export interface CoursePart {
+interface CoursePartBase {
     name: string;
     exerciseCount: number;
-}
+  }
+  
+  interface CoursePartBasic extends CoursePartBase {
+    description: string;
+    kind: "basic"
+  }
+  
+  interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number;
+    kind: "group"
+  }
+  
+  interface CoursePartBackground extends CoursePartBase {
+    description: string;
+    backgroundMaterial: string;
+    kind: "background"
+  }
+  
+  export type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
